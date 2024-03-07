0ddf3a0a6971d704599dad3abe07ff16c37f95a0
Simon <simon.leary42@gmail.com>
2024-03-06 21:39:09 -0500

    update path

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ from torchvision.transforms import ToTensor
 import torch
 from PIL import Image
 from IPython.display import display
-ckpt = '../lightning_logs/version_0/checkpoints/epoch=151-step=57151-val_ExpRate=0.6365.ckpt'
+ckpt = 'lightning_logs/version_0/checkpoints/epoch=151-step=57151-val_ExpRate=0.6365.ckpt'
 #model = LitCoMER.load_from_checkpoint(ckpt, map_location="cpu")
 model = LitCoMER.load_from_checkpoint(ckpt)
 model = model.eval()
